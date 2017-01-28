3d19f38e5e81d4d28dd74a5b11776ec87303dbbc
Sam Swanke <skswanke@Sams-MacBook-Air.local>
2017-01-28 17:33:31 -0500

    Initialize main class

---
+++ main.java
@@ -0,0 +1,30 @@
+/*
+Go Fish main class
+
+Go fish rules:
+Five cards are dealt from a standard 52-card deck to each player, or seven cards if there are four or fewer players.
+The remaining cards are shared between the players, usually spread out in a disorderly pile referred to as the "ocean"
+or "pool".
+The player whose turn it is to play asks another player for his or her cards of a particular face value. For example
+Alice may ask, "Bob, do you have any threes?" Alice must have at least one card of the rank she requested. Bob must
+hand over all cards of that rank if possible. If he has none, Bob tells Alice to "go fish" (or simply "fish"), and Alice
+draws a card from the pool and places it in her own hand. Then it is the next player's turn – unless the card Alice drew
+is the card she asked for, in which case she shows it to the other players, and she gets another turn. When any player
+at any time has all four cards of one face value, it forms a book, and the cards must be placed face up in front of that
+player.
+Play proceeds to the left. When all sets of cards have been laid down in books, the game ends. The player with the
+most books wins.
+
+Possible classes:
+Deck
+Hand
+Player
+AI
+Pool
+
+*/
+class main {
+    public static void main(String[] args) {
+        System.out.println("Go Fish");
+    }
+}
