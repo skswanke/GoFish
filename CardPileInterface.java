a759072742b2a1959e64e014f94e51e7055f8d1a
skswanke <skswanke@users.noreply.github.com>
2017-02-03 12:19:51 -0500

    Create CardPileInterface.java

---
+++ CardPileInterface.java
@@ -0,0 +1,11 @@
+/*
+* Sam Swanke
+* CS 110
+* Card Pile interface
+*/
+
+public interface CardPileInterface {
+	public void shuffle ();
+	public void addCard (Card c);
+	public Card drawCard ();
+}
