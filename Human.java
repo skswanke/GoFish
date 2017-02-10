fcdf5f914ecc9367e6c190e849cc1bdc8bedb439
Sam Swanke <skswanke@ip0af50e04.int.uvm.edu>
2017-02-10 17:52:06 -0500

    stuff to merge

--- Human.java
+++ Human.java
@@ -1,5 +1,26 @@
 class Human extends Player {
-      public Human() {
-          
-      }
+    public Human(Hand newHand) {
+        super(newHand);
+    }
+
+    public Deck turn(Deck deck){
+        System.out.print("Your cards are: ");
+        for(int i = 0; i<hand.size(); i++){
+            System.out.print(hand[i].getRank().toString() + hand[i].getRank().toString() + ", ");
+        }
+        System.out.println("\nChoose a rank to ask for: ");
+        Scanner scanner = new Scanner(System.in);
+        String askRank = scanner.nextLine();
+        aiCard = getReply(askRank);
+        if(aiCard != null){
+            hand.addCard(aiHasCard);
+        } else {
+            System.out.println("Sorry AI doesn't have that card! Go Fish!");
+            cardRecieved = goFish();
+            System.out.println("You got " + cardRecieved + "!");
+        }
+    }
+    private getReply(Card c){
+        AI.reply();
+    }
 }
